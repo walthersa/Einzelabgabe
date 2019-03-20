2a229bb2efe89b7bc170cd55ba339bce60791bb4
Walther Salcher <walthersalcher@aon.at>
2019-03-20 16:55:50 +0100

    Button

--- app/src/main/java/com/example/einzelabgabe/MainActivity.java
+++ app/src/main/java/com/example/einzelabgabe/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.einzelabgabe;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView textView = (TextView) findViewById(R.id.textViewAntwort);
+        final EditText editText = (EditText) findViewById(R.id.editText);
+
+        final Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                EditText editText = (EditText) findViewById(R.id.editText);
+                textView.setText(editText.getText().toString());
+            }
+        });
     }
 }
