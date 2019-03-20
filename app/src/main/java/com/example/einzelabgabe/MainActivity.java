035afb05d7886cd7d714bd914f33ac67792f6cb9
Walther Salcher <walthersalcher@aon.at>
2019-03-20 17:38:34 +0100

    Serververbindung... funkt noch nicht

--- app/src/main/java/com/example/einzelabgabe/MainActivity.java
+++ app/src/main/java/com/example/einzelabgabe/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.einzelabgabe;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,22 +8,70 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+public class MainActivity extends AppCompatActivity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final TextView textView = (TextView) findViewById(R.id.textViewAntwort);
+
         final EditText editText = (EditText) findViewById(R.id.editText);
+
+
 
         final Button button = findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                EditText editText = (EditText) findViewById(R.id.editText);
-                textView.setText(editText.getText().toString());
+            public void onClick(View v)  {
+
+                new Connection().execute();
+
+
             }
         });
+    }
+
+    public class Connection extends AsyncTask<Void, Void, Void>{
+
+        protected Void doInBackground(Void... arg0){
+            final TextView textView = (TextView) findViewById(R.id.textViewAntwort);
+
+            EditText editText = (EditText) findViewById(R.id.editText);
+            textView.setText(editText.getText().toString());
+
+            String sentence;
+            String modifiedSentence;
+
+            // BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+
+            Socket clientSocket = null;
+            try {
+                clientSocket = new Socket("se2-isys.aau.at",53212);
+
+                DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+
+                BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+                //   sentence = inFromUser.readLine();
+
+                outToServer.writeBytes(editText.getText().toString() + '\n');
+
+                modifiedSentence = inFromServer.readLine();
+
+                System.out.println("From Server: " + modifiedSentence);
+
+                clientSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+
+            }
+            return null;
+        }
     }
 }
